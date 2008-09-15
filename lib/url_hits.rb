397dfc789e194013a1a10fd452124826b36638f3
Wes Oldenbeuving <narnach@gmail.com>
2008-09-15 15:44:06 +0200

    UrlHits is a kind of Array now, to make statistical analysis easier

--- lib/url_hits.rb
+++ lib/url_hits.rb
@@ -1,37 +1,54 @@
 require 'float_ext'
-require 'array_ext'
 
-class UrlHits
-  attr_reader :url, :hit_times
+class UrlHits < Array
+  attr_reader :url
   
   def initialize(url)
     @url = url
-    @hit_times = []
   end
-  
-  def method_missing(meth,*args,&block)
-    if [:sum, :median, :stddev, :size].include? meth
-      hit_times.send(meth,*args,&block)
-    else
-      super(meth,*args,&block)
-    end
+
+  def add_hit(time)
+    self << time
+  end
+
+  def to_s
+    '%6i %s %s' % [size, time_stats, @url]
   end
 
   def avg
     sum / size.to_f
   end
 
-  def add_hit(time)
-    @hit_times << time
+  def median
+    return 0 if size == 0
+    if size%2==0
+      # Average two middle values
+      # [1,2,3,4,5,6].median #=> 3.5
+      (self[size / 2] + self[size / 2 - 1]) / 2
+    else
+      # Use middle value
+      # [1,2,3,4,5].median #=> 3
+      self[size / 2]
+    end
   end
+
+  def stddev
+    mean = sum / size.to_f
+    diffs = map {|n| n - mean }
+    sqdiffs = diffs.map {|n| n * n }
+    sqsum = sqdiffs.sum
+    sqmean = sqsum / size.to_f
+    Math::sqrt(sqmean)
+  end
+
+  def sum
+    inject(0.0) {|s,n| s+n }
+  end
+
+  protected
 
   def time_stats
     return nil unless size > 0
-    low, high = hit_times.min, hit_times.max
-    '%3.03f %3.03f %3.03f %3.03f %3.03f %6.03f' % [low, median, avg, stddev, high, sum]
-  end
-
-  def to_s
-    '%6i %s %s' % [size, time_stats, @url]
+    '%3.03f %3.03f %3.03f %3.03f %3.03f %6.03f' % [min, median, avg, stddev, max, sum]
   end
 end
